f6fecd58c0201f78f1d0f234118d390b93c4f8ad
DeveloperDemetri <developerdemetri@gmail.com>
2017-03-24 11:40:54 -0700

    working on run tab and controller separation

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -1,6 +1,4 @@
-angular.module('ZooPhy').controller('resultsController', function ($scope, RecordData) {
-
-  //TODO fix updating data via factory
+angular.module('ZooPhy').controller('resultsController', function ($scope, $http, RecordData) {
 
   $scope.recordsPerPage = 25;
   $scope.pageNums = [25, 50, 100, 250, 500];
@@ -8,6 +6,14 @@ angular.module('ZooPhy').controller('resultsController', function ($scope, Recor
   $scope.numSelected = RecordData.getNumSelected();
   $scope.results = RecordData.getRecords();
   $scope.displayedResults = RecordData.getRecords();
+
+  $scope.$watch(function () {return RecordData.getRecords();}, function (newValue, oldValue) {
+        if (newValue !== oldValue) {
+          $scope.results = newValue;
+          $scope.groupIsSelected = false;
+          $scope.loadDetails(newValue[0].accession);
+        }
+  }, true);
 
   $scope.loadDetails = function(accession) {
     $http.get(SERVER_URI+'/record?accession='+accession.trim()).then(function(response) {
@@ -34,17 +40,31 @@ angular.module('ZooPhy').controller('resultsController', function ($scope, Recor
 
   $scope.toggleAll = function() {
     for (let i = 0; i < $scope.results.length; i++) {
-      $scope.results[i].includeInJob = !$scope.groupIsSelected;
+      $scope.results[i].includeInJob = $scope.groupIsSelected;
     }
     if ($scope.groupIsSelected) {
-      $scope.numSelected = 0;
-    }
-    else {
       $scope.numSelected = $scope.results.length;
     }
-    $scope.groupIsSelected = !$scope.groupIsSelected;
+    else {
+      $scope.numSelected = 0;
+    }
     RecordData.setNumSelected($scope.numSelected);
   };
 
-
 });
+
+/*
+Copyright 2017 ASU Biodesign Center for Environmental Security's ZooPhy Lab
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
--- public/javascripts/run.js
+++ public/javascripts/run.js
@@ -1,5 +1,56 @@
-angular.module('ZooPhy').controller('runController', function ($scope, RecordData) {
+angular.module('ZooPhy').controller('runController', function ($scope, $http, RecordData) {
+
+  const EMAIL_RE = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   $scope.numSelected = RecordData.getNumSelected();
+  $scope.jobEmail;
+  $scope.jobName;
+
+  $scope.$watch(function () {return RecordData.getNumSelected();}, function (newValue, oldValue) {
+        if (newValue !== oldValue) {
+          $scope.numSelected = newValue;
+        }
+  });
+
+  $scope.runJob = function() {
+    if ($scope.jobEmail && EMAIL_RE.test($scope.jobEmail)) {
+      let jobAccessions = [];
+      let records = RecordData.getRecords();
+      for (let i = 0; i < records; i++) {
+        if (records[i].includeInJob) {
+          jobAccessions.push(records[i].accession);
+        }
+      }
+      let runUri = SERVER_URI+'/job/run';
+      let jobData = {
+        replyEmail: $scope.jobEmail,
+        jobName: $scope.jobName,
+        accessions: jobAccessions,
+        useGLM: false,
+        predictors: null
+      };
+      //$http.post(runUri, jobData)
+      console.log('job started...');
+    }
+    else {
+      console.log('invalid email')
+    }
+  };
 
 });
+
+/*
+Copyright 2017 ASU Biodesign Center for Environmental Security's ZooPhy Lab
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
