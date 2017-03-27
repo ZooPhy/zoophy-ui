86dcf14d99fb6f5855f72448bdc35b838cd10242
DeveloperDemetri <developerdemetri@gmail.com>
2017-03-27 11:38:04 -0700

    added in testing setup

---
+++ test/bin_tools.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let express = require('express');
+let app = require('../app');
+let request = require('supertest');
+let assert = require('chai').assert;
+
+describe('Init Test', function() {
+  it('Bin Tools test ran', function(done) {
+    done();
+  });
+});
---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let express = require('express');
+let app = require('../app');
+let request = require('supertest');
+let assert = require('chai').assert;
+
+describe('Init Test', function() {
+  it('Index test ran', function(done) {
+    done();
+  });
+});
