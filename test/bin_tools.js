ef088005dfefa506394af40819ffe789f32bc483
DeveloperDemetri <developerdemetri@gmail.com>
2017-03-28 09:45:29 -0700

    finished GBRecord tests

--- test/bin_tools.js
+++ test/bin_tools.js
@@ -14,13 +14,34 @@ describe('GebBankRecord Tests', function() {
   it('Should create valid Index record', function(done) {
     let rawRecord = helperData.luceneRecord;
     let indexRecord = new GenBankRecord.LuceneRecord(rawRecord);
-    assert.equal('CY214007', indexRecord.accession, 'Should contain correct Accession');
+    assert.strictEqual('CY214007', indexRecord.accession, 'Should contain correct Accession');
+    assert.isString(indexRecord.genes, 'Should contain String of Genes');
+    assert.strictEqual('PB2', indexRecord.genes, 'Should contain correct Gene name');
+    assert.strictEqual('Influenza A', indexRecord.virus, 'Should contain correct Virus name');
+    assert.isString(indexRecord.date, 'Should contain String date');
+    assert.strictEqual('03-Jan-2017', indexRecord.date, 'Should contain correct date')
+    assert.strictEqual('homo sapiens; gender f; age 76', indexRecord.host, 'Should contain correct Host name');
+    assert.strictEqual('United States', indexRecord.country, 'Should contain correct Country');
+    assert.strictEqual(2316, indexRecord.segmentLength, 'Should contain correct Segment Length');
+    assert.strictEqual(false, indexRecord.includeInJob, 'Should not be included in Job');
     done();
   });
   it('Should create valid DB record', function(done) {
     let rawRecord = helperData.sqlRecord;
     let dbRecord = new GenBankRecord.SQLRecord(rawRecord);
-    assert.equal('CY215262', dbRecord.accession, 'Should contain correct Accession');
+    assert.strictEqual('CY215262', dbRecord.accession, 'Should contain correct Accession');
+    assert.strictEqual('04-Jan-2017', dbRecord.date, 'Should contain correct Date')
+    assert.isDefined(dbRecord.pubmedID, 'Should contain empty pubmedID');
+    assert.strictEqual('n/a', dbRecord.pubmedID, 'Should contain correct n/a pubmedID')
+    assert.strictEqual('Influenza A virus (A/Maryland/02/2017(H1N1)) Viruses; ssRNA viruses; ssRNA negative-strand viruses; Orthomyxoviridae; Influenzavirus A.', dbRecord.virus , 'Should contain correct Virus');
+    assert.strictEqual(1948908, dbRecord.taxon , 'Should contain correct Taxon');
+    assert.strictEqual('A/Maryland/02/2017', dbRecord.strain , 'Should contain correct Strain');
+    assert.strictEqual('Unknown', dbRecord.isolate , 'Should contain Unknown Isolate');
+    assert.strictEqual('Homo sapiens; gender M; age 3', dbRecord.host , 'Should contain correct Host name');
+    assert.strictEqual('maryland,US', dbRecord.location , 'Should contain correct Location');
+    assert.isString(dbRecord.genes, 'Should contain String of Genes');
+    assert.strictEqual('NS', dbRecord.genes , 'Should contain correct Gene names');
+    assert.strictEqual('Influenza A virus (A/Maryland/02/2017(H1N1)) nuclear export protein (NEP) and nonstructural protein 1 (NS1) genes, complete cds.', dbRecord.definition , 'Should contain correct Definition');
     done();
   });
 });
