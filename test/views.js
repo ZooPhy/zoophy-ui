fb4c7b7297b9eede6e524e54d68795252e1fe345
DeveloperDemetri <developerdemetri@gmail.com>
2017-03-28 10:01:39 -0700

    added views test

--- test/views.js
+++ test/views.js
@@ -3,10 +3,36 @@
 let express = require('express');
 let app = require('../app');
 let request = require('supertest');
-let assert = require('chai').assert;
+let chai = require('chai');
+let sinon = require('sinon');
+let ejs = require('ejs');
 
 describe('Init Test', function() {
-  it('Views test ran', function(done) {
-    done();
+  let spy = sinon.spy(ejs, '__express');
+  it('Should get Lost status', function(done) {
+    request(app)
+      .get('/derp')
+      .end(function(err, res) {
+        if (err) done(err);
+        chai.assert.strictEqual(404, res.status, 'Page Not Found');
+        chai.expect(res.body).to.be.empty;
+        chai.expect(spy.calledWithMatch(/\/views\/header\.ejs$/)).to.be.false;
+        chai.expect(spy.calledWithMatch(/\/views\/home\.ejs$/)).to.be.false;
+        chai.expect(spy.calledWithMatch(/\/views\/search\.ejs$/)).to.be.false;
+        chai.expect(spy.calledWithMatch(/\/views\/results\.ejs$/)).to.be.false;
+        chai.expect(spy.calledWithMatch(/\/views\/run\.ejs$/)).to.be.false;
+        done();
+      });
+  });
+  it('Should Get Home Page', function(done) {
+    request(app)
+      .get('/')
+      .end(function(err, res) {
+        if (err) done(err);
+        chai.assert.strictEqual(200, res.status, 'Page Found');
+        chai.assert.isNotNull(res.body, 'Got Home Page');
+        chai.expect(spy.calledWithMatch(/\/views\/home\.ejs$/)).to.be.true;
+        done();
+    });
   });
 });
