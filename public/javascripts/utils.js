116c573a734b3863952836baef3f8306a4b40559
kbhangal <kbhangal@asu.edu>
2019-01-31 15:21:14 -0700

    move server_url param to utils

---
+++ public/javascripts/utils.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//TODO: update on server if using reverse proxy
+//var SERVER_URI = 'https://zodo.asu.edu/zoophy';   //main-site
+//var SERVER_URI = 'https://zodo.asu.edu/zoophy/test';   //test-site
+var MAIN_URI = location.protocol + '//' + location.hostname + ':' + location.port;
