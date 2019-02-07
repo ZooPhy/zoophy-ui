16d561bba8725c4b24b76686e97c73c5095d09d7
kbhangal <36016440+kbhangal@users.noreply.github.com>
2019-02-07 16:36:45 -0700

    Delete utils.js

--- public/javascripts/utils.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-//TODO: update on server if using reverse proxy
-//var SERVER_URI = 'https://zodo.asu.edu/zoophy';   //main-site
-//var SERVER_URI = 'https://zodo.asu.edu/zoophy/test';   //test-site
-var MAIN_URI = location.protocol + '//' + location.hostname + ':' + location.port;
